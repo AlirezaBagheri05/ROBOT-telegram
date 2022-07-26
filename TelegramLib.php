8ae794e781fe728e626094228bd3d6f998df3f5f
Alireza <alirezabagheri3461@gmail.com>
2022-07-26 20:25:31 +0430

    making telegramlib

--- TelegramLib.php
+++ TelegramLib.php
@@ -8,7 +8,11 @@ class TelegramLib
     {
         self::$url = 'https://api.telegram.org/bot'.TOKEN.'/';
     }
-
+    /** 
+     * 
+     * 
+     * type : array or bool
+     */
     private static function execute(string $_method,array $_parameters):array|bool
     {
         if(!isset(self::$url)){
@@ -22,6 +26,27 @@ class TelegramLib
         if(!empty($_parameters)){
             curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($_parameters));
         }
+        curl_setopt($curl,CURLOPT_HEADER,['content-Type:application/json']); 
+
+        $update_result = curl_exec($curl);
+        $error = null;
+        if(curl_errno($curl)){
+            $error = curl_errno($curl);
+        }
+        if(!is_null($error)){
+            return false;
+        }
+
+        return json_decode($result,true);
+
     }
 
+    public static function send_message(string $_text, string $_chat_id):array|bool
+    {
+        $parameters = [
+            "text" = $_text,
+            "chat_id" = $_chat_id
+        ];
+        return  self::execute('sendMessage',$parameters,);
+    }
 }
