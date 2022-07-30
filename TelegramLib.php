433bbb08cecc9ea4367982d7dff447bf62caa88e
Alireza <alirezabagheri3461@gmail.com>
2022-07-30 21:25:20 +0430

    made lib of telegram bot

--- TelegramLib.php
+++ TelegramLib.php
@@ -23,35 +23,47 @@ class TelegramLib
         }
 
         $url = self::$url . $_method;
-        $url = $url.'?text='.$_parameters['text'].'&'.'chat_id='.$_parameters['chat_id'];
         $curl = curl_init($url);
+        if(!empty($_parameters)){
+            curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($_parameters));
+        }
+        curl_setopt($curl,CURLOPT_HTTPHEADER,['content-Type:application/json']); 
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
         $update_result = curl_exec($curl);
-        // curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-        // if(!empty($_parameters)){
-        //     curl_setopt($curl,CURLOPT_POSTFIELDS,json_encode($_parameters));
-        // }
-        // curl_setopt($curl,CURLOPT_HEADER,['content-Type:application/json']); 
+        $error = null;
+        if(curl_errno($curl)){
+            $error = curl_errno($curl);
+        }
+        if(!is_null($error)){
+            return false;
+        }
 
-        // $update_result = curl_exec($curl);
-        // $error = null;
-        // if(curl_errno($curl)){
-        //     $error = curl_errno($curl);
-        // }
-        // if(!is_null($error)){
-        //     var_dump($error);
-        //     return false;
-        // }
-
-        return json_decode($update_result,true);
+        $output =  json_decode($update_result,true);
+        if(is_null($output)){
+            return false;
+        }
+        return $output;
 
     }
 
-    public static function send_message(string $_text, string $_chat_id)
+    public static function send_message(string $_text, string $_chat_id):array|bool
     {
         $parameters = [
             "text" => $_text,
             "chat_id" => $_chat_id
         ];
         return  self::execute('sendMessage',$parameters);
+    }
+    public static function get_update(int $offset = null):array|null
+    {
+        $parameters = [];
+        if(!is_null($offset)){
+            $parameters["offset"] = $offset;
+        }
+        $result = self::execute('getUpdates',$parameters);
+        if(!is_array($result)){
+            return $result['result'];
+        }
+
     }
 }
